e43ad0f205b9bb9af85f0c97f60fe2dc281a2b55
Mattia Asti <mattia.asti@11fs.com>
2019-10-18 10:13:07 +0100

    Add client authentication

--- client/src/index.js
+++ client/src/index.js
@@ -1,28 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { ApolloClient } from 'apollo-client';
-import { InMemoryCache } from 'apollo-cache-inmemory';
-import { WebSocketLink } from 'apollo-link-ws';
-import { HttpLink } from 'apollo-link-http';
-import { onError } from 'apollo-link-error';
-import { ApolloLink, split } from 'apollo-link';
-import { ApolloProvider } from 'react-apollo';
-import { getMainDefinition } from 'apollo-utilities';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-
+import React from "react";
+import ReactDOM from "react-dom";
+import { ApolloClient } from "apollo-client";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { WebSocketLink } from "apollo-link-ws";
+import { HttpLink } from "apollo-link-http";
+import { onError } from "apollo-link-error";
+import { ApolloLink, split } from "apollo-link";
+import { ApolloProvider } from "react-apollo";
+import { getMainDefinition } from "apollo-utilities";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
 const wsLink = new WebSocketLink({
   uri: `ws://localhost:4000/graphql`,
   options: {
     reconnect: true
+  },
+  connectionParams: {
+    authToken: "HELLO_KEN"
   }
 });
 
 // Create an http link:
 const httpLink = new HttpLink({
-  uri: 'http://localhost:4000/graphql'
+  uri: "http://localhost:4000/graphql"
 });
 
 // using the ability to split links, you can send data to each link
@@ -31,10 +33,10 @@ const link = split(
   // split based on operation type
   ({ query }) => {
     const { kind, operation } = getMainDefinition(query);
-    return kind === 'OperationDefinition' && operation === 'subscription';
+    return kind === "OperationDefinition" && operation === "subscription";
   },
   wsLink,
-  httpLink,
+  httpLink
 );
 
 const client = new ApolloClient({
@@ -43,8 +45,8 @@ const client = new ApolloClient({
       if (graphQLErrors)
         graphQLErrors.map(({ message, locations, path }) =>
           console.log(
-            `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`,
-          ),
+            `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
+          )
         );
       if (networkError) console.log(`[Network error]: ${networkError}`);
     }),
@@ -59,7 +61,7 @@ const Root = () => (
   </ApolloProvider>
 );
 
-ReactDOM.render(<Root />, document.getElementById('root'));
+ReactDOM.render(<Root />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
